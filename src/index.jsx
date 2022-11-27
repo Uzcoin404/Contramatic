5e2ac3db5ab9821dd86a729d82965d7262beac73
Your Name <uzcointg@gmail.com>
2022-11-27 11:56:39 +0500

    moved to contrmatic.com

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider as LangProvider } from "./context/langContext";
-import { Provider as DataProvider } from "./context/dataContext";
-import AuthProvider from "./context/userContext";
-import App from "./App";
+// import ReactDOM from "react-dom/client";
+// import { Provider as LangProvider } from "./context/langContext";
+// import { Provider as DataProvider } from "./context/dataContext";
+// import AuthProvider from "./context/userContext";
+// import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <LangProvider>
-            <DataProvider>
-                <AuthProvider>
-                    <App />
-                </AuthProvider>
-            </DataProvider>
-        </LangProvider>
-    </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//     <React.StrictMode>
+//         <LangProvider>
+//             <DataProvider>
+//                 <AuthProvider>
+//                     <App />
+//                 </AuthProvider>
+//             </DataProvider>
+//         </LangProvider>
+//     </React.StrictMode>
+// );
+window.location.href = "https://contramatic.com/";
